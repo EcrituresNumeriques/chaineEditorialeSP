1a151278591d621923422de342c220b3a2d3868e
PookMook <arthur.juchereau@gmail.com>
2017-07-06 12:06:16 -0400

    upload/download

--- yamlEditor/src/js/DOMselector.js
+++ yamlEditor/src/js/DOMselector.js
@@ -1,14 +1,14 @@
 import YAML from 'js-yaml';
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
-import { App } from './components/App.jsx'
-import _ from 'lodash'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
+import { App } from './components/App.jsx';
+import _ from 'lodash';
 
 
 let yaml = document.querySelector("#source");
-let js = document.querySelector("#result");
+//let js = document.querySelector("#result");
 let reactForm = document.querySelector(".app");
 
 let SexyYamlType = new YAML.Type('!sexy', {
@@ -37,8 +37,8 @@ export function handleDOMchanges() {
 
   yaml.addEventListener("input",_.debounce(YAMLupdate,500));
   yaml.addEventListener("click",function(){active_element = "yaml"});
-  js.addEventListener("input",_.debounce(JSupdate,500));
-  js.addEventListener("click",function(){active_element = "js"});
+  //js.addEventListener("input",_.debounce(JSupdate,500));
+  //js.addEventListener("click",function(){active_element = "js"});
   reactForm.addEventListener("click",function(){active_element = "react"});
 
 
@@ -47,24 +47,62 @@ export function handleDOMchanges() {
     //console.log('updating Yaml');
   }
 
-  function updateJS(){
-    js.value = JSON.stringify(store.getState().obj,false,1);
-    //console.log('updating JS');
-  }
+  //function updateJS(){
+  //  js.value = JSON.stringify(store.getState().obj,false,1);
+  //  //console.log('updating JS');
+  //}
 
   function YAMLupdate(){
     let obj = YAML.load(yaml.value, { schema: SEXY_SCHEMA });
     store.dispatch({type:"YAML_UPDATE",obj:obj});
   }
 
-  function JSupdate(){
-    let obj = JSON.parse(js.value);
-    store.dispatch({type:"JS_UPDATE",obj:obj});
-  }
+//  function JSupdate(){
+//    let obj = JSON.parse(js.value);
+//    store.dispatch({type:"JS_UPDATE",obj:obj});
+//  }
 
 
   function objUpdate(){
     if(active_element != "yaml"){updateYAML();}
-    if(active_element != "js"){updateJS();}
+//    if(active_element != "js"){updateJS();}
   }
 }
+
+//Drag and drop module
+var dropZone = document.querySelector('#drop');
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var files = evt.dataTransfer.files;
+    var reader = new FileReader();
+    reader.onload = function(event) {
+         yaml.value = event.target.result;
+         yaml.dispatchEvent(new Event('input'));
+         removeDragClass();
+    };
+    reader.readAsText(files[0],"UTF-8");
+  }
+
+//On dragover stance, specify copy of textfile
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+  }
+
+//when entering the dragover stance, add CSS class
+function addDragClass(){
+    dropZone.className = "draghover";
+  }
+//when leaving the dragover stance, remove the CSS class
+  function removeDragClass(){
+    dropZone.className = "";
+  }
+
+  // Setup the dnd listeners.
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+  dropZone.addEventListener('dragenter', addDragClass, false);
+  dropZone.addEventListener('dragleave', removeDragClass, false);
