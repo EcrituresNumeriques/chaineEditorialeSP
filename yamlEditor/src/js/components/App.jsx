853e075e98ee4fa710bf58b5c5a5f55dbc078a8d
PookMook <arthur.juchereau@gmail.com>
2017-07-12 10:39:22 -0400

    add dossier management

--- yamlEditor/src/js/components/App.jsx
+++ yamlEditor/src/js/components/App.jsx
@@ -3,6 +3,7 @@ import { TextInput } from './TextInput.jsx';
 import { SelectInput } from './SelectInput.jsx';
 import { Resumes} from './Resumes.jsx';
 import { Authors } from './Authors.jsx';
+import { Dossier } from './Dossier.jsx';
 import { Reviewers } from './Reviewers.jsx';
 import { Collaborateurs} from './Collaborateurs.jsx';
 import { MotsClefs} from './MotsClefs.jsx';
@@ -19,6 +20,7 @@ export function App(){
       <TextInput target="subtitle" title="Sous-titre" />
       <Date target="date" title="Date"/>
       <Resumes/>
+      <Dossier />
       <Authors />
       <Reviewers />
       <Keywords/>
