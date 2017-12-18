eded4b6c9a48556de6d140fdb4eaaeb44bdf9666
PookMook <arthur.juchereau@gmail.com>
2017-12-17 21:16:13 -0500

    Auteurs

--- yamlEditor/src/js/components/Authors.jsx
+++ yamlEditor/src/js/components/Authors.jsx
@@ -4,14 +4,14 @@ import { store } from '../redux/store.js';
 import React from 'react';
 import _ from 'lodash';
 
-export function Authors(){
-  let authors = _.get(store.getState().obj,"authors",[]);
+export function Authors(props){
+  let authors = _.get(props.state,"authors",[]);
   let targetNewAuthor = authors.length;
   return(
     <section>
       <h1><i className="fa fa-users" aria-hidden="true"></i> Auteurs</h1>
-      {authors.map((o,i)=>(<Author key={i} index={i} />))}
-      <p className="addToArray" onClick={function(){store.dispatch({type:"FORM_UPDATE",target:"authors["+targetNewAuthor+"]",value: {"forname": "","surname": "","orcid": "","viaf": "","foaf": "","isni": "","wikidata": ""}})}}><i className="fa fa-user-plus" aria-hidden="true"></i> Ajouter un auteur</p>
+      {authors.map((o,i)=>(<Author key={i} index={i} state={props.state} updateState={props.updateState}/>))}
+      <p className="addToArray" onClick={function(){props.updateState({"forname": "","surname": "","orcid": "","viaf": "","foaf": "","isni": "","wikidata": ""},"authors["+targetNewAuthor+"]")}}><i className="fa fa-user-plus" aria-hidden="true"></i> Ajouter un auteur</p>
     </section>
   )
 }
@@ -19,13 +19,13 @@ export function Authors(){
 function Author(props){
   return(
     <section className="group">
-      <TextInput target={"authors["+props.index+"].surname"} title="Nom"/>
-      <TextInput target={"authors["+props.index+"].forname"} title="Prénom"/>
-      <TextInput target={"authors["+props.index+"].orcid"} title="Orcid"/>
-      <TextInput target={"authors["+props.index+"].viaf"} title="VIAF"/>
-      <TextInput target={"authors["+props.index+"].foaf"} title="FOAF"/>
-      <TextInput target={"authors["+props.index+"].isni"} title="ISNI"/>
-      <TextInput target={"authors["+props.index+"].wikidata"} title="Wikidata"/>
+      <TextInput target={"authors["+props.index+"].surname"} title="Nom" state={props.state} updateState={props.updateState}/>
+      <TextInput target={"authors["+props.index+"].forname"} title="Prénom" state={props.state} updateState={props.updateState}/>
+      <TextInput target={"authors["+props.index+"].orcid"} title="Orcid" state={props.state} updateState={props.updateState}/>
+      <TextInput target={"authors["+props.index+"].viaf"} title="VIAF" state={props.state} updateState={props.updateState}/>
+      <TextInput target={"authors["+props.index+"].foaf"} title="FOAF" state={props.state} updateState={props.updateState}/>
+      <TextInput target={"authors["+props.index+"].isni"} title="ISNI" state={props.state} updateState={props.updateState}/>
+      <TextInput target={"authors["+props.index+"].wikidata"} title="Wikidata" state={props.state} updateState={props.updateState}/>
     </section>
   )
 }
