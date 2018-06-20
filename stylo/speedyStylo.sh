f3f31ba7a5740792ae4619da34e202e697d50d0f
marviro <marcello@vitalirosati.net>
2018-06-20 17:19:52 -0400

    ajout rename images téléchargées

--- stylo/speedyStylo.sh
+++ stylo/speedyStylo.sh
@@ -5,10 +5,21 @@ unzip $1.zip
 rename "s/${2}/${1}/g" *
 sed -i -e "s/\/${2}/${1}/g" $1.yaml
 pandoc --standalone --filter pandoc-citeproc --template=/home/marcello/Desktop/sp/git/chaineEditorialeSP/templates/templateLaTeX.latex -f markdown -t latex $1.md $1.yaml -o $1.md.tex
-cp -r ../media ./
 sed -i -e 's/https:\/\/i\.imgur.com\//media\//g' $1.md
 sed -i -e 's/https:\/\/i\.imgur.com\//media\//g' $1.html
 sed -i -e 's/https:\/\/i\.imgur.com\//media\//g' $1.md.tex
+cd media
+COUNTER=0
+for filename in "$3"*; do
+    COUNTER=$[$COUNTER +1]
+    echo "${filename%.*}" 
+    sed -i -e "s/${filename%.*}/img${COUNTER}/g" ../$1.md
+    sed -i -e "s/${filename%.*}/img${COUNTER}/g" ../$1.md.tex
+    sed -i -e "s/${filename%.*}/img${COUNTER}/g" ../$1.html
+    mv ${filename%.*}.${filename##*.} img${COUNTER}.${filename##*.}
+done
+cd ..
+cp -r ../media ./
 pdflatex $1.md.tex
 rm $1.zip
-git status
+#git status
